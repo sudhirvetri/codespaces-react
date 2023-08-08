da16e40f2eb5cb34c61fab1ef6c07ebd2dc9dacf
sudhir <sudhirvetri@gmail.com>
2023-08-08 04:47:12 +0000

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            edit by sudhir
           </a>
         </p>
       </header>
